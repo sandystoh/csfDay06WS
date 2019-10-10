f8eede0939f7abd3dc4b42d16a82e417e6a0bf0a
Sandy Toh <sandytoh@u.nus.edu>
2019-10-10 14:43:33 +0800

    Travis back to GH Pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'http://api.openweathermap.org/data/2.5/weather',
+  api_url: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather',
   api_key: '8cf5fa7f056c9d29e4721bf7fab83ffb'
 };
